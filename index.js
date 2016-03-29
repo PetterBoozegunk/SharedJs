fb4c0a05e906c6a8b460b013d7538c5221c384fe
Petter Ahlberg <petter.ahlberg@gmail.com>
2016-03-29 14:43:27 +0200

    Trying to work out how to write tests...

---
+++ index.js
@@ -0,0 +1,16 @@
+﻿/*jslint node: true, stupid: true */
+
+var concatFiles = require("concat-files"),
+
+    sharedJs = {
+        concat: function (srcArray, dest) {
+
+            console.log("concatFiles: ", arguments);
+
+            concatFiles(srcArray, dest, function () {
+                console.log("concatFiles: done");
+            });
+        }
+    };
+
+module.exports = sharedJs;
---
+++ test/tests.js
@@ -0,0 +1,22 @@
+﻿/*jslint node: true, stupid: true */
+
+var expect = require("chai").expect,
+
+    fs = require("fs"),
+    sharedJs = require("../index.js");
+
+describe("Concat test", function () {
+    it("works", function () {
+        sharedJs.concat(["js/concat1.js", "js/concat2.js"], "/testDest");
+
+        var hasCreatedTestDest = false;
+
+        fs.lstat("/testDest", function (err, stats) {
+            if (!err && stats.isDirectory()) {
+                hasCreatedTestDest = true;
+            }
+        });
+
+        expect(hasCreatedTestDest).to.equal(true);
+    });
+});
